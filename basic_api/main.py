c83a5c2e6e711001a8648710662a82667fb3eb8b
phernandez <phernandez@basicmachines.co>
2023-06-07 15:00:25 -0500

    add htmx

--- basic_api/main.py
+++ basic_api/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request
+from asgi_htmx import HtmxMiddleware
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.add_middleware(HtmxMiddleware)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -11,6 +13,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse, name="index")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/result", name="result")
+async def result(request: Request) -> Response:
+    assert (htmx := request.scope["htmx"])
+    template = "partials/result.html"
+    context = {"request": request, "table": [{"name":"foo", "value": "bar"}]}
+    return templates.TemplateResponse(template, context)
